68d0db4425c44e4b33d3e81d1bd542c6856104ee
jadenkeener <jadenkeener@gmail.com>
2023-04-27 09:48:52 -0600

    same as last commit but synced with server

--- giffer.py
+++ giffer.py
@@ -3,14 +3,14 @@ import glob
 
 def giffer(path):
     frames = []
-    for file in glob.glob(f"{path}/*.png"):
+    for file in sorted(glob.glob(f"{path}/*.png")):
         frames.append(Image.open(file))
   
     frame_one = frames[0]
     frame_one.save("timelapse.gif", format="GIF", append_images=frames, 
-                    save_all=True, duration=50, loop=0)
+                    save_all=True, duration=150, loop=0)
                     
                     
                     
 if __name__ == "__main__":
-    giffer(".")
+    giffer("./pngs")
